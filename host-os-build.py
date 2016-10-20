01231d4a7edaf2e3eff7827cd6caf920c8b846c8
Fabiano Rosas <farosas@linux.vnet.ibm.com>
2016-10-20 11:47:28 -0200

    Use Repo class to clone versions repository
    This removes code duplication and allows the versions repository to be
    setup by scripts other than host-os-build.py

--- host-os-build.py
+++ host-os-build.py
@@ -20,62 +20,11 @@ from lib import config
 from lib import exception
 from lib import log_helper
 from lib import manager
+from lib import repository
 
 import pygit2
 
 LOG = logging.getLogger(__name__)
-
-
-def get_reference(repository, short_reference_string):
-    """
-    Get repository reference (branch, tag) based on a short reference
-    suffix string.
-    """
-    prefixes = ["refs/tags", "refs/heads", "refs/remotes"]
-    for remote in repository.remotes:
-        prefixes.append(os.path.join("refs/remotes", remote.name))
-    for prefix in prefixes:
-        reference_string = os.path.join(prefix, short_reference_string)
-        LOG.debug("Trying to get reference: %s", reference_string)
-        try:
-            return repository.lookup_reference(reference_string)
-        except KeyError:
-            pass
-    else:
-       raise exception.RepositoryError(
-           message="Reference not found in repository")
-
-def setup_versions_repository(versions_git_url, dest, version):
-    # Load if it is an existing git repo
-    if os.path.exists(dest):
-        try:
-            versions_repo = pygit2.Repository(dest)
-            LOG.info("Found versions repository!")
-
-        except KeyError:
-            raise exception.RepositoryError(
-                message="Failed to setup Versions repository")
-    else:
-        LOG.info("Cloning into %s..." % dest)
-        versions_repo = pygit2.clone_repository(versions_git_url,
-                                                dest)
-
-    version_reference = get_reference(versions_repo, version)
-    LOG.info("Trying to check out versions' reference: %s",
-             version_reference.name)
-    try:
-        for remote in versions_repo.remotes:
-            remote.fetch()
-            LOG.info("Fetched changes for %s" % remote.name)
-
-        versions_repo.checkout(
-            version_reference, strategy=pygit2.GIT_CHECKOUT_FORCE)
-        versions_repo.reset(versions_repo.head.target, pygit2.GIT_RESET_HARD)
-    except ValueError:
-        LOG.error("Failed to check out %s", version_reference.name)
-        raise exception.RepositoryError(
-            message="Could not find reference %s on versions repo"
-            % version_reference)
 
 
 def main(args):
@@ -88,10 +37,11 @@ def main(args):
     log_helper.LogHelper(logfile=conf.get('default').get('log_file'),
                          verbose=conf.get('default').get('verbose'))
     try:
-        version = conf.get('default').get('build_version')
-        setup_versions_repository(
-            conf.get('default').get('build_versions_repository_url'),
-            config.COMPONENTS_DIRECTORY, version)
+        # setup versions directory
+        path, dirname = os.path.split(config.COMPONENTS_DIRECTORY)
+        repository.Repo(
+            dirname, conf.get('default').get('build_versions_repository_url'),
+            path, conf.get('default').get('build_version'))
 
         conf['default']['packages'] = conf['default']['packages'] if (
             conf.get('default').get('packages')) else (
